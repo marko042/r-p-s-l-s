a1bc7393c57f444bbf53c2b2e605710c724792f4
marko042 <marko.sormaz1211@gmail.com>
2023-02-21 17:51:33 +0000

    Add funtion that generates computer choice based on five different selections and dispay it inside html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,42 @@
+// selects all the elements by Id and button elements
 
-
-// selects all the elements by Id
-const computerChoiceDisplay = document.getElementById('computer-choice') 
-const userChoiceDisplay = document.getElementById('user-choice') 
-const resultDisplay = document.getElementById('result') 
-const possibleChoices = document.querySelectorAll('button') 
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
 
 //grabs the user choice and stores it
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id                                                                   
+    userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
-
+    generateCompChoice()
 }))
+
+function generateCompChoice() {
+    const randomNumber = Math.floor(Math.random() * 5) +1 //alternatively use possibleChoices.lenght
+
+    if (randomNumber === 1 ) {
+        computerChoice = 'rock'
+    }
+
+    if (randomNumber === 2 ) {
+        computerChoice = 'paper'
+    }
+
+    if (randomNumber === 3 ) {
+        computerChoice = 'scissors'
+    }
+
+    if (randomNumber === 4 ) {
+        computerChoice = 'lizard'
+    }
+
+    if (randomNumber === 5 ) {
+        computerChoice = 'spock'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+
+}
