849769fbb92a89217f02a05cba69c167ba48e855
marko042 <marko.sormaz1211@gmail.com>
2023-02-23 00:24:51 +0000

    Final check and  save progress on readme.md file

--- README.md
+++ README.md
@@ -22,3 +22,100 @@ This website represents a modification of a popular game rock paper scissors mad
 
 
         ![Heading text](/assets/images/docs/heading-text.PNG)
+
+        - Buttons section
+            - Each button represents a choice for the user to select and depict a hand gesture in a form of  corresponding text shown below
+
+            ![buttons](/assets/images/docs/buttons.PNG)
+
+            - Choices and result sections
+                -   this section tracks the player's and computer's selection and logs a result of each combination
+
+                ![choices and result](/assets/images/docs/choices-and-result.PNG)
+
+
+            - Rules and history
+                - This section contains a brief explanation of the game origins and also the basic rules of the game.
+
+                ![history and rules](/assets/images/docs/rules-and-history.PNG)
+
+                ## Features left to implement
+                - As I progress my coding skills I would like to to expand the game by tracking the resuls and showing history previous moves made by the player and computer
+
+
+
+                # Testing
+
+                Testing was performed throughout the entire build. Using Firefox  developer tools while building to pinpoint and troubleshoot any issues as I went along.
+                Javascript tutor website was used for better understanding of how the code is processed.
+
+                ## Validator Testing
+
+                - HTML [ValidatorW3](https://validator.w3.org/)
+                    - No errors were found
+
+                - CSS [Jigsaw.W3](https://jigsaw.w3.org/css-validator/validator)
+                     - The CSS validator did not find any errors <p>
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" />
+    </a>
+</p>
+
+- [JSHint](https://jshint.com)
+    - No errors found
+
+- Lighthouse
+    - ![lighthouse report](/assets/images/docs/lighthouse-report.PNG)
+
+
+    - Deployed website was also tested on the following devices with no apparent bugs
+        - Xiaomi Redmi 9 Android
+        - iPhone SE 2016
+        - iPhone 13
+        - MacBook Pro M1
+        - iPad A13
+        - MSI GF63 Thin
+        - Dell P2719H
+        - HP E24 G4
+                
+    - Browsers tested with no significant bugs
+        - Mozilla Firefox
+        - Opera
+        - Chrome
+        - Edge
+        - Brave
+        - Safari
+
+    ## Deployment
+
+    - The site was deployed to GitHub pages. The steps to deploy are as follows: 
+        - In the GitHub repository, navigate to the Settings tab
+        - From the source section drop-down menu, select the Master Branch
+        - Once the master branch has been selected, the page will be automatically refreshed with a detailed ribbon display to indicate the successful deployment.
+
+        The live link can be found on [r-p-s-l-s](https://marko042.github.io/r-p-s-l-s/)
+
+## Credits
+
+In this section I will to best of my abilities and most comprehensively list all the resources that contributed in any way to building this website.
+
+# Content
+
+ - Portion of the development of this website was done using resources provided in love maths walkthrough project
+ - Numerous helpful tips were found on [W3shools online web tutorials](https://www.w3schools.com/)
+ - Great tips on [Codecademy/Java](https://www.codecademy.com/resources/docs/java)
+ - [Codepen](https://codepen.io) with its amazing three tab layout
+ - also to mention several youtube videos that I found extremely helpful  
+ - [3 ways to code Rock Paper Scissors in JavaScript (Beginner to Intermediate to Advanced!)](https://www.youtube.com/watch?v=RwFeg0cEZvQ) 
+ - [How To Code Rock Paper Scissors In JavaScript](https://www.youtube.com/watch?v=1yS-JV4fWqY)
+ - [Javascript Rock Paper Scissors Game Tutorial | Web Development Tutorial](https://www.youtube.com/watch?v=qWPtKtYEsN4)
+ - [Google Fonts](https://fonts.google.com/) was facilitated for all the font needs.
+ - Color palette courtesy of [Canva](https://www.canva.com/)
+ ![color palette by Canva](/assets/images/docs/canva-color-palette.PNG)
+
+ # Media
+  - Text used in history and rules section was taken from [](https://bigbangtheory.fandom.com/wiki/Rock,_Paper,_Scissors,_Lizard,_Spock)
+  - Buttons taken from [Free Icons Library](https://icon-library.com/)
+  
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,13 @@ const possibleChoices = document.querySelectorAll('button');
 
 
 
-
-
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => { // add event listener to button clicks
     let userChoice = e.target.id;
     generateComputerChoice(userChoice, getResult);
-    
+
 }));
 
-//Declares a function for computer choice
+
 
 
 function generateComputerChoice(userChoice, showResult) {
@@ -23,57 +21,57 @@ function generateComputerChoice(userChoice, showResult) {
 }
 
 
-    // Gets the result based on the choices made by user and computer and displays it
-    function getResult(computerChoice, userChoice) {
-        let result = 'calculating...';
-        if (computerChoice === userChoice) {
-            result = "it's a tie, pick again!";
-        } else if (computerChoice === 'scissors' && userChoice === 'paper') { //comparing computer choice and user choice to create result message
-            result = "Scissors cuts Paper!"; //comparing computer choice and user choice to create result message
-        } else if (computerChoice === 'paper' && userChoice === 'rock') {
-            result = "Paper covers Rock!";
-        } else if (computerChoice === 'rock' && userChoice === 'scissors') {
-            result = "Rock crushes Scissors!";
-        } else if (computerChoice === 'rock' && userChoice === 'lizard') {
-            result = "Rock crushes Lizard!";
-        } else if (computerChoice === 'lizard' && userChoice === 'spock') {
-            result = "Lizard poisons Spock!";
-        } else if (computerChoice === 'spock' && userChoice === 'scissors') {
-            result = "Spock smashes Scissors!";
-        } else if (computerChoice === 'scissors' && userChoice === 'lizard') {
-            result = "Scissors decapitates Lizard!";
-        } else if (computerChoice === 'lizard' && userChoice === 'paper') {
-            result = "Lizard eats Paper!";
-        } else if (computerChoice === 'paper' && userChoice === 'spock') {
-            result = "Paper disproves Spock!";
-        } else if (computerChoice === 'spock' && userChoice === 'rock') {
-            result = "Spock vaporizes Rock";
-        } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-            result = "Scissors cuts Paper!";
-        } else if (userChoice === 'paper' && computerChoice === 'rock') {
-            result = "Paper covers Rock!";
-        } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-            result = "Rock crushes Scissors!";
-        } else if (userChoice === 'rock' && computerChoice === 'lizard') {
-            result = "Rock crushes Lizard!";
-        } else if (userChoice === 'lizard' && computerChoice === 'spock') {
-            result = "Lizard poisons Spock!";
-        } else if (userChoice === 'spock' && computerChoice === 'scissors') {
-            result = "Spock smashes Scissors!";
-        } else if (userChoice === 'scissors' && computerChoice === 'lizard') {
-            result = "Scissors decapitates Lizard!";
-        } else if (userChoice === 'lizard' && computerChoice === 'paper') {
-            result = "Lizard eats Paper!";
-        } else if (userChoice === 'paper' && computerChoice === 'spock') {
-            result = "Paper disproves Spock!";
-        } else if (userChoice === 'spock' && computerChoice === 'rock') {
-            result = "Spock vaporizes Rock";
-        }
-
-        // Display everything
-        document.getElementById('user-choice').innerHTML = userChoice;
-        console.log(result);
-        document.getElementById('result').innerHTML = result;
-        document.getElementById('computer-choice').innerHTML = computerChoice;
-
+// Gets the result based on the choices made by user and computer and displays it
+function getResult(computerChoice, userChoice) {
+    let result = 'calculating...';
+    if (computerChoice === userChoice) {
+        result = "it's a tie, pick again!";
+    } else if (computerChoice === 'scissors' && userChoice === 'paper') { //comparing computer choice and user choice to create result message
+        result = "Scissors cuts Paper!"; //comparing computer choice and user choice to create result message
+    } else if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = "Paper covers Rock!";
+    } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = "Rock crushes Scissors!";
+    } else if (computerChoice === 'rock' && userChoice === 'lizard') {
+        result = "Rock crushes Lizard!";
+    } else if (computerChoice === 'lizard' && userChoice === 'spock') {
+        result = "Lizard poisons Spock!";
+    } else if (computerChoice === 'spock' && userChoice === 'scissors') {
+        result = "Spock smashes Scissors!";
+    } else if (computerChoice === 'scissors' && userChoice === 'lizard') {
+        result = "Scissors decapitates Lizard!";
+    } else if (computerChoice === 'lizard' && userChoice === 'paper') {
+        result = "Lizard eats Paper!";
+    } else if (computerChoice === 'paper' && userChoice === 'spock') {
+        result = "Paper disproves Spock!";
+    } else if (computerChoice === 'spock' && userChoice === 'rock') {
+        result = "Spock vaporizes Rock";
+    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        result = "Scissors cuts Paper!";
+    } else if (userChoice === 'paper' && computerChoice === 'rock') {
+        result = "Paper covers Rock!";
+    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        result = "Rock crushes Scissors!";
+    } else if (userChoice === 'rock' && computerChoice === 'lizard') {
+        result = "Rock crushes Lizard!";
+    } else if (userChoice === 'lizard' && computerChoice === 'spock') {
+        result = "Lizard poisons Spock!";
+    } else if (userChoice === 'spock' && computerChoice === 'scissors') {
+        result = "Spock smashes Scissors!";
+    } else if (userChoice === 'scissors' && computerChoice === 'lizard') {
+        result = "Scissors decapitates Lizard!";
+    } else if (userChoice === 'lizard' && computerChoice === 'paper') {
+        result = "Lizard eats Paper!";
+    } else if (userChoice === 'paper' && computerChoice === 'spock') {
+        result = "Paper disproves Spock!";
+    } else if (userChoice === 'spock' && computerChoice === 'rock') {
+        result = "Spock vaporizes Rock";
     }
+
+    // Display everything
+    document.getElementById('user-choice').innerHTML = userChoice;
+    console.log(result);
+    document.getElementById('result').innerHTML = result;
+    document.getElementById('computer-choice').innerHTML = computerChoice;
+
+}
